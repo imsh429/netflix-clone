cd8cf353508263be8363d0a956dd8c3528390c6c
imsh429 <sh99429@naver.com>
2025-12-03 16:38:14 +0900

    deploy.yml추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/netflix-demo/', // 이 줄 추가!
   plugins: [
     vue(),
     vueDevTools(),
